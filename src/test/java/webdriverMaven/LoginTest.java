96bb45d89d0abdb1e644d04a39a3bec841a66275
Attila | QA | Wanari <attila@wanari.com>
2021-12-17 15:24:19 +0100

    Initial Commit

---
+++ src/test/java/webdriverMaven/LoginTest.java
@@ -0,0 +1,43 @@
+package webdriverMaven;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+	public static WebDriver driver;
+	
+	
+	@BeforeSuite
+	public void setUp() {
+		
+		driver = new FirefoxDriver();
+		
+		
+	}
+	
+	@Test
+	public void doLogin() throws InterruptedException {
+		
+		
+		driver.get("http://gmail.com");
+		driver.findElement(By.id("identifierId")).sendKeys("trainer@way2automation.com");
+		driver.findElement(By.cssSelector("button[jsname = 'LgbsSe']")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.cssSelector("input[type = 'password']")).sendKeys("asdf");
+		
+	}
+	
+	
+	@AfterSuite
+	public void tearDown() {
+		
+		driver.quit();
+		
+	}
+	
+}
